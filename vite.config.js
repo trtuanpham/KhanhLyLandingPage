88073c4255fb6e4f4310f8ad231248c42349581e
Tuấn Phạm <tr.tuanpham@gmail.com>
2026-03-18 00:23:49 +0700

    Add new assets and update HTML structure
    - Added new image assets: lab-testing.avif, project-bridge.webp, project-urban-bridge.webp, supervision.avif, and survey-engineer.jpg.
    - Updated index.html to reference new JavaScript and CSS files.
    - Created a new index.html file in the root directory.
    - Modified vite.config.js to customize output file names for assets.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   base: "/",
   build: {
     outDir: "docs",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
   },
   plugins: [vue()],
 });
